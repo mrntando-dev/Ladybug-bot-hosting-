df741d8cf5f3807f3c2cb0e4be6cd1f58e92cf8c
mrntando-dev <sibonginkosincust@gmail.com>
2025-11-10 07:48:01 +0200

    Create Transaction.js

---
+++ backend/models/Transaction.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['credit', 'debit'],
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
