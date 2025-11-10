aa803e3a163954b609b03f8b26884f3efb01c732
mrntando-dev <sibonginkosincust@gmail.com>
2025-11-10 11:37:48 +0200

    Delete backend directory

--- backend/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-const admin = async (req, res, next) => {
-  if (!req.user.isAdmin) {
-    return res.status(403).json({ error: 'Access denied. Admin only.' });
-  }
-  next();
-};
-
-module.exports = admin;
